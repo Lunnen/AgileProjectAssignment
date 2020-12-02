d0b91e9e0aae82235c158747d143134f40c4a2f3
JonatanGlader <jonatan.glader@utb.ecutbildning.se>
2020-12-02 15:03:32 +0100

    Realease 0.3

---
+++ 0 Releases/Ver 0.3 - Graphical update/js/fileupload.js
@@ -0,0 +1,33 @@
+/*-----------------------------------------------------
+Replace "input type=file" button with a custom button
+*/
+const customBtn = document.getElementById("custom-button");
+
+//virtually clicks the input button
+customBtn.addEventListener("click", function () {
+  input.click();
+});
+
+var input = document.getElementById("inputFile");
+input.addEventListener("change", showDataURI);
+
+function showDataURI() {
+  var output = document.getElementById("output");
+  var file = input.files[0];
+
+  var reader = new FileReader();
+  reader.onload = (e) => {
+    imgContainers.push(
+      new ImgContainer(
+        prompt("Enter Category", ""),
+        e.target.result, //sets value of raw-data-input in src.
+        "",
+        "",
+        ""
+      )
+    );
+    createFilterButtons(); //Create a new filter button, if needed
+    filterSelection("all"); //updates rendering of images
+  };
+  reader.readAsDataURL(file);
+}
