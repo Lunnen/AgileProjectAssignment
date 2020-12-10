58230527bfbd717b04f4043f123c9e3a3e43a5bd
JonatanGlader <jonatan.glader@utb.ecutbildning.se>
2020-12-10 19:01:22 +0100

    V0.51

---
+++ 0 Releases/Ver 0.51 - Graphical update/js/fileupload.js
@@ -0,0 +1,37 @@
+/*-----------------------------------------------------
+Replace "input type=file" button with a custom button
+*/
+const customBtn = document.getElementById("custom-button");
+
+//virtually clicks the input button
+customBtn.addEventListener("click", function () {
+  input.click();
+});
+
+var input = document.getElementById("inputFile");
+input.addEventListener("change", showDataURI);
+
+function showDataURI() {
+  var output = document.getElementById("output");
+  var file = input.files[0];
+
+  var reader = new FileReader();
+  reader.onload = (e) => {
+    imgContainers.push(
+      new ImgContainer(
+        prompt("Image category: ", "NewFilter"),
+        e.target.result, //sets value of raw-data-input in src.
+        prompt("Alternative name (if image cannot load): ", "alt"),
+        prompt("Title: ", "Title"),
+        prompt("Description: ", "Description")
+      )
+    );
+
+    localStorage.setItem("pureData", JSON.stringify(stringDataToSave)); //save the data locally as a string.
+
+    filterSelection("all"); //updates rendering of images
+    createFilterButtons(); //Create a new filter button, if needed
+    updateItems(); //updates items in the "dropEvents"
+  };
+  reader.readAsDataURL(file);
+}
