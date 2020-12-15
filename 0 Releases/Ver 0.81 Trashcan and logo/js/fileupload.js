c11d0c65fe67467563ee46137a519f28705889e3
Christer Holm <ch@star78.se>
2020-12-15 14:50:44 +0100

    Trash and logo

---
+++ 0 Releases/Ver 0.81 Trashcan and logo/js/fileupload.js
@@ -0,0 +1,37 @@
+"use strict";
+/*-----------------------------------------------------
+Replace "input type=file" button with a custom button
+*/
+const customBtn = document.getElementById("custom-button");
+
+//virtually clicks the input button
+customBtn.addEventListener("click", function () {
+  input.click();
+});
+
+var input = document.getElementById("inputFile");
+input.addEventListener("change", showDataURI);
+
+function showDataURI() {
+  var output = document.getElementById("output");
+  var file = input.files[0];
+
+  var reader = new FileReader();
+  reader.onload = (e) => {
+    imgContainers.push(
+      new ImgContainer(
+        prompt("Image category: ", "NewFilter"),
+        e.target.result, //sets value of raw-data-input in src.
+        parseInt(Date.now() * Math.random()), //prompt("Alternative name (if image cannot load): ", "alt"),
+        prompt("Title: ", "Title"),
+        prompt("Description: ", "Description")
+      )
+    );
+
+    saveData(); //Saves everything in imgContainers to LocalStorage inside client browser.
+    filterSelection("all"); //updates rendering of images
+    createFilterButtons(); //Create a new filter button, if needed
+    updateItems(); //updates items in the "dropEvents"
+  };
+  reader.readAsDataURL(file);
+}
---
+++ 0 Releases/Ver 0.81 Trashcan and logo/js/web.js
@@ -0,0 +1,136 @@
+"use strict";
+
+// Declare Elements
+let divMain = document.createElement("div");
+let pageLogo = document.createElement("img");
+let filterBtnContainer = document.createElement("div");
+let galleryMain = document.createElement("div");
+let uploadForm = document.createElement("form");
+let uploadInput = document.createElement("input");
+let uploadButton = document.createElement("img");
+let dropToDelete = document.createElement("img");
+//let dropToDeleteHover = document.createElement("img");
+// Put elements in these boxes on web-page
+document.body.appendChild(divMain);
+divMain.appendChild(pageLogo);
+divMain.appendChild(filterBtnContainer);
+divMain.appendChild(uploadForm);
+uploadForm.appendChild(uploadInput);
+uploadForm.appendChild(uploadButton);
+divMain.appendChild(dropToDelete);
+//dropToDelete.onmouseenter=function(){dropToDelete.src="./meny/deletehover.png"}
+//dropToDelete.onmouseout=function(){dropToDelete.src="./meny/delete.png"}
+//divMain.appendChild(dropToDeleteHover);
+divMain.appendChild(galleryMain);
+
+// Text + img content
+pageLogo.src = "./meny/headloga.png";
+pageLogo.className = "pageLogo";
+galleryMain.className = "row";
+divMain.className = "main";
+filterBtnContainer.className = "myBtnContainer";
+uploadForm.id = "myForm";
+uploadInput.type = "file";
+uploadInput.id = "inputFile";
+uploadInput.hidden = "hidden";
+uploadButton.src = "./meny/upload.png";
+uploadButton.id = "custom-button";
+uploadButton.textContent = "Upload an image";
+dropToDelete.src = "./meny/delete.png";
+dropToDelete.className = "dropToDelete";
+//dropToDeleteHover.src = "./meny/deletehover.png";
+//dropToDeleteHover.className= "dropToDeleteHover";
+
+// The object that creates a Card with text.
+function ImgContainer(enterCategory, imgSrc, altName, title, text) {
+  //checking if a string is null/undefined, blank or contains only white-space.
+  if (!enterCategory || /^\s*$/.test(enterCategory)) {
+    this.className = "unfiltered";
+  } else {
+    this.className = enterCategory.toLowerCase();
+  }
+  this.imgSrc = imgSrc;
+  this.altName = altName;
+  this.title = title;
+  this.text = text;
+
+  let galleryCard = document.createElement("div");
+  let galleryContent = document.createElement("div");
+  let galleryIMG = document.createElement("img");
+  let galleryTitle = document.createElement("h4");
+  let galleryText = document.createElement("p");
+
+  galleryContent.className = "content";
+  galleryCard.className = "column " + this.className;
+  galleryCard.draggable = "true";
+  galleryIMG.src = imgSrc;
+  galleryIMG.alt = altName;
+  galleryIMG.style = "width: 100%";
+  galleryTitle.textContent = title;
+  galleryText.textContent = text;
+
+  galleryMain.appendChild(galleryCard);
+  galleryCard.appendChild(galleryContent);
+  galleryContent.appendChild(galleryIMG);
+  galleryContent.appendChild(galleryTitle);
+  galleryContent.appendChild(galleryText);
+
+  this.getImgCategory = function () {
+    return this.className;
+  };
+}
+
+var imgContainers = []; // Array were IMG "cards" are stored.
+
+//SOME HARDCODED STUFF - REMOVAL BEFORE RELEASE!!
+/*---------------------------------------------------
+This is where you push the pictures into the array (imgContainers), 
+which then renders them on page 
+*/
+
+/*
+imgContainers.push(
+  new ImgContainer(
+    "Landscape",
+    "./images/Ees96A.jpg",
+    "nature pic",
+    "Forest",
+    "this is a nice pic"
+  )
+);
+imgContainers.push(
+  new ImgContainer(
+    "Landscape",
+    "./images/close-up-nature02.jpg",
+    "forestal view",
+    "Forestal view",
+    "another one"
+  )
+);
+imgContainers.push(
+  new ImgContainer(
+    "Landscape",
+    "./images/QGucjH.jpg",
+    "beautiful",
+    "A field and clouds",
+    "Another one bites the dust..."
+  )
+);
+imgContainers.push(
+  new ImgContainer(
+    "Computer",
+    "https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "Inside",
+    "Inside",
+    "Calm inside view URL FROM NET"
+  )
+);
+imgContainers.push(
+  new ImgContainer(
+    "Monument",
+    "https://images.unsplash.com/photo-1606210122158-eeb10e0823bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+    "Petra city in the dark",
+    "Petra",
+    "URL FROM NET"
+  )
+); */
