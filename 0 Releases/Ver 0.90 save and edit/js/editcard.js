66b747c6cefa404bf4fa1e35906f5a59da57e453
Christer Holm <ch@star78.se>
2020-12-17 16:22:19 +0100

    Save And Edit

---
+++ 0 Releases/Ver 0.90 save and edit/js/editcard.js
@@ -0,0 +1,15 @@
+/*
+let cards=document.getElementsByClassName("column");
+
+ for ( let card of cards ){
+    for (var i = 0; i < card.childNodes.length; i++) {
+
+        let child = card.childNodes[i].className == "editButton";
+
+        child.addEventListener("click", function () {
+            console.log("Clicked");
+            card.clicked();      
+        });
+   }
+}
+*/
